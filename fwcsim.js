19f6f7530448a8c7dd0e3db67928108cd91d5e70
Philip Steiner <philip@psteiner.com>
2018-07-07 15:54:25 -0700

    First commit

---
+++ fwcsim.js
@@ -0,0 +1,30 @@
+var groups = [
+    { A: [{ team: "Russia", rank: 0 }, { team: "Saudi Arabia", rank: 0 }, { team: "Egypt", rank: 0 }, { team: "Uruguay", rank: 0 }] },
+    { B: [{ team: "Portugal", rank: 0 }, { team: "Spain", rank: 0 }, { team: "Morocco", rank: 0 }, { team: "Iran", rank: 0 }] },
+    { C: [{ team: "France", rank: 0 }, { team: "Australia", rank: 0 }, { team: "Peru", rank: 0 }, { team: "Denmark", rank: 0 }] },
+    { D: [{ team: "Argentina", rank: 0 }, { team: "Iceland", rank: 0 }, { team: "Croatia", rank: 0 }, { team: "Nigeria", rank: 0 }] },
+    { E: [{ team: "Brazil", rank: 0 }, { team: "Switzerland", rank: 0 }, { team: "Costa Rica", rank: 0 }, { team: "Serbia", rank: 0 }] },
+    { F: [{ team: "Germany", rank: 0 }, { team: "Mexico", rank: 0 }, { team: "Sweden", rank: 0 }, { team: "South Korea", rank: 0 }] },
+    { G: [{ team: "Belgium", rank: 0 }, { team: "Panama", rank: 0 }, { team: "Tunisia", rank: 0 }, { team: "England", rank: 0 }] },
+    { H: [{ team: "Poland", rank: 0 }, { team: "Senegal", rank: 0 }, { team: "Colombia", rank: 0 }, { team: "Japan", rank: 0 }] },
+];
+
+function tournament() {
+    console.log("Play the tournament");
+}
+
+function groupPlay() {
+    console.log("Group Play");
+}
+
+function roundOfSixteen() {
+    console.log("Round of Sixteen");
+}
+
+function quarterFinal() {
+    console.log("Quarter Final");
+}
+
+function semiFinal() {
+    console.log("Semi Final")
+}
